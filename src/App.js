9f7f157e29ba79266f9f93aaa1c8f0edd37c87da
ProfesorV <xavierfisico@yahoo.com>
2022-07-13 21:48:27 -0400

    7/13/22

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import NavigationBar from "./components/NavigationBar";
-import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <NavigationBar/>
-      <Routes>
-        <Route path='/' element={<Hero/>}/>
-        <Route path='/experience' element={<Experience/>}/>
-      </Routes>
+      <Hero/>
+      <Experience/>
     </div>
   );
 }
